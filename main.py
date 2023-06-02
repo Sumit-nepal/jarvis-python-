a58b1b9a28318eacc7fe1d432a62cc55b517163b
Sumit-Nepal <nepalsumit30@gmail.com>
2023-06-02 12:45:58 +0545

    added commands

--- main.py
+++ main.py
@@ -1,57 +1,107 @@
-"""
-This is AI based personal assistance (Jarvis) which will automate
-the day to day task. 
-"""
-import pyttsx3
-import speech_recognition as sr
+import pyttsx3 #pip install pyttsx3
+import speech_recognition as sr #pip install speechRecognition
 import datetime
+import wikipedia #pip install wikipedia
+import webbrowser
+import os
+import smtplib
 
-engine = pyttsx3.init("sapi5")  # initialize the voice
-voices = engine.getProperty("voices")
-engine.setProperty("voice",voices[0].id)
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
 
 
-def speak(message):
-    engine.say(message)
+def speak(audio):
+    engine.say(audio)
     engine.runAndWait()
 
 
-def wishme():
-    hour = int(datetime.datetime.now().hour) # gets the current time from the device
-    if (hour >= 0) and (hour < 12):
-        speak("Good Morning")
-    
-    elif (hour >= 12) and (hour < 18):
-        speak("Good Afternoon")
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")   
 
     else:
-        speak("Good Evening")
+        speak("Good Evening!")  
 
-    speak("Hello This is jarvis, how may i help you today")
+    speak("Hello this is jarvis how may i help you today")       
 
-def takecommand():
-    """
-    It takes the audio input from the mic of the device and returns the string
-    output 
-    """
-    
+def takeCommand():
+    #It takes microphone input from the user and returns string output
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
-        r.pause_threshold = 1  # wait for input upto 1 seconds
-        message = r.listen(source)
-    
-    try:
-        print("Reading Commands..")
-        query = r.recognize_google(message, language="en-us")
-        print(f"Command: {query}\n")
+        r.pause_threshold = 1
+        audio = r.listen(source)
 
-    except Exception:
-        speak("I didn't get that sir")
-        print("Say that again please")
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        # print(e)    
+        print("Say that again please...")  
         return "None"
     return query
 
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('youremail@gmail.com', 'your-password')
+    server.sendmail('youremail@gmail.com', to, content)
+    server.close()
+
 if __name__ == "__main__":
-    wishme()
-    takecommand()
+    wishMe()
+    while True:
+    # if 1:
+        query = takeCommand().lower()
+
+        # Logic for executing tasks based on query
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+        elif 'open stackoverflow' in query:
+            webbrowser.open("stackoverflow.com")   
+
+
+        elif 'play music' in query:
+            music_dir = 'D:\\Non Critical\\songs\\Favorite Songs2'
+            songs = os.listdir(music_dir)
+            print(songs)    
+            os.startfile(os.path.join(music_dir, songs[0]))
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+            speak(f"Sir, the time is {strTime}")
+
+        elif 'open code' in query:
+            codePath = "C:\\Users\\sumit\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            os.startfile(codePath)
+
+        elif 'email to sumit' in query:
+            try:
+                speak("What should I say?")
+                content = takeCommand()
+                to = "nepalsumit30@gmail.com"    
+                sendEmail(to, content)
+                speak("Email has been sent!")
+            except Exception as e:
+                print(e)
+                speak("Sorry Sir. I am not able to send this email")    
